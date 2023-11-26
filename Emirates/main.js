94b51f80a545b3bc38d211e94161abfcfe203025
Zenhal789 <Zenhalzenu789@gmail.com>
2023-11-26 10:33:26 +0530

    7

---
+++ Emirates/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $(".about-btn").click(function() {
+        $(".about-us").toggle(); 
+    });
+});
+$(document).ready(function() {
+    $(".help-btn").click(function() {
+        $(".help").toggle(); 
+    });
+});
+$(document).ready(function() {
+    $(".book-btn").click(function() {
+        $(".book").toggle(); 
+    });
+});
+$(document).ready(function() {
+    $(".manage-btn").click(function() {
+        $(".manage").toggle(); 
+    });
+});
+$(document).ready(function() {
+    $(".before-btn").click(function() {
+        $(".before").toggle(); 
+    });
+});
+$(document).ready(function() {
+    $(".where-btn").click(function() {
+        $(".where").toggle(); 
+    });
+});
+$(document).ready(function() {
+    $(".exp-btn").click(function() {
+        $(".experience").toggle(); 
+    });
+});
+$(document).ready(function() {
+    $(".loyalty-btn").click(function() {
+        $(".loyalty").toggle(); 
+    });
+});
